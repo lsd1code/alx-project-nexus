28de7a015bb1883cf4cb8bcc8d89a831f392cb6c
Lesedi Thibile (lsd1code) <lesedi.xys11@gmail.com>
2025-09-11 20:02:06 +0200

    DOCS: add swagger for API documentation

--- api/urls.py
+++ api/urls.py
@@ -12,15 +12,15 @@ urlpatterns = [
     path("", index, name="index"),
     path("token/", TokenObtainPairView.as_view(), name="token_obatain_pair"),
     path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
-    # path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path(
-    #     "schema/swagger-ui/",
-    #     SpectacularSwaggerView.as_view(url_name="schema"),
-    #     name="swagger-ui",
-    # ),
-    # path(
-    #     "schema/redoc/",
-    #     SpectacularRedocView.as_view(url_name="schema"),
-    #     name="redoc",
-    # ),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
