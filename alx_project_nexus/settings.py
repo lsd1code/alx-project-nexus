13f72369725d5a7edac496d9bb4cd64b7efeba25
lesedi thibile <lesedi.xys11@gmail.com>
2025-09-23 20:29:09 +0200

    CHORE: apply quick bug fixes

--- alx_project_nexus/settings.py
+++ alx_project_nexus/settings.py
@@ -164,7 +164,8 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        # "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": os.getenv("REDIS_EXTERNAL_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
